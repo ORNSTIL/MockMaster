844ae9374e9067336779ff86ea0e104b3cd15bba
Sam Todd <stodd03@calpoly.edu>
2024-05-13 02:23:45 -0700

    implemented search and removed files

--- src/api/players.py
+++ src/api/players.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends, Path
 from pydantic import BaseModel
+from enum import Enum
 import sqlalchemy
 from src import database as db
 from src.api import auth
@@ -13,9 +14,142 @@ router = APIRouter(
 class PlayerStatsResponse(BaseModel):
     player_stats: dict
 
+class DraftPlayerRequest(BaseModel):
+    team_id: int
+
+class search_sort_options(str, Enum):
+    player_name = "player_name"
+    position = "position"
+    team = "team"
+    age = "age"
+    standard_fantasy_points = "standard_fantasy_points"
+    ppr_fantasy_points = "ppr_fantasy_points"
+
+class search_sort_order(str, Enum):
+    asc = "asc"
+    desc = "desc" 
+
+@router.get("/search/")
+def search_players(
+    player_name: str = "",
+    position: str = "",
+    team: str = "",
+    search_page: str = "",
+    sort_col: search_sort_options = search_sort_options.ppr_fantasy_points,
+    sort_order: search_sort_order = search_sort_order.desc
+):
+    
+    # set which page of results to get
+    if search_page == "":
+        offset = 0
+        prev_token = ""
+    else:
+        offset = (int(search_page)-1)*10
+        if offset == 0:
+            prev_token = ""
+        else:
+            prev_token = str(int(search_page)-1)
+
+    # set sort order
+    if sort_col is search_sort_options.player_name:
+        if sort_order is search_sort_order.asc:
+            order_by = db.players.c.player_name
+        else:
+            order_by = sqlalchemy.desc(db.players.c.player_name)
+    elif sort_col is search_sort_options.position:
+        if sort_order is search_sort_order.asc:
+            order_by = db.stats.c.position
+        else:
+            order_by = sqlalchemy.desc(db.stats.c.position)
+    elif sort_col is search_sort_options.team:
+        if sort_order is search_sort_order.asc:
+            order_by = db.stats.c.team
+        else:
+            order_by = sqlalchemy.desc(db.stats.c.team)
+    elif sort_col is search_sort_options.age:
+        if sort_order is search_sort_order.asc:
+            order_by = db.stats.c.age
+        else:
+            order_by = sqlalchemy.desc(db.stats.c.age)
+    elif sort_col is search_sort_options.standard_fantasy_points:
+        if sort_order is search_sort_order.asc:
+            order_by = db.stats.c.fantasy_points_standard_10
+        else:
+            order_by = sqlalchemy.desc(db.stats.c.fantasy_points_standard_10)
+    elif sort_col is search_sort_options.ppr_fantasy_points:
+        if sort_order is search_sort_order.asc:
+            order_by = db.stats.c.fantasy_points_ppr_10
+        else:
+            order_by = sqlalchemy.desc(db.stats.c.fantasy_points_ppr_10)
+    else:
+        assert False
+
+    # set sqlalchemy statement
+    stmt = (
+        sqlalchemy
+        .select(
+            db.players.c.player_id,
+            db.players.c.player_name,
+            db.stats.c.position,
+            db.stats.c.team,
+            db.stats.c.age,
+            db.stats.c.fantasy_points_standard_10,
+            db.stats.c.fantasy_points_ppr_10
+        )
+        .select_from(
+            db.players
+            .join(db.stats, db.players.c.player_id == db.stats.c.player_id)
+        )
+        .limit(11)
+        .offset(offset)
+        .order_by(order_by)
+    )
+
+    # filter only if player_name parameter and/or position paramater are passed
+    if player_name != "":
+        stmt = stmt.where(db.players.c.player_name.ilike(f"%{player_name}%"))
+    if position != "":
+        stmt = stmt.where(db.stats.c.position.ilike(f"%{position}%"))
+    if team != "":
+        stmt = stmt.where(db.stats.c.team.ilike(f"%{team}%"))
+
+    # connect to database
+    with db.engine.begin() as connection:
+        result = connection.execute(stmt).fetchall()
+        json = []
+        # set next page
+        if len(result) > 10:
+            result = result[0:10]
+            if search_page == "":
+                next_token = "2"
+            else:
+                next_token = str(int(search_page)+1)
+        else:
+            next_token = ""
+        for row in result:
+            json.append(
+                {
+                    "player_id": row.player_id,
+                    "player_name": row.player_name,
+                    "position": row.position,
+                    "team": row.team,
+                    "age": row.age,
+                    "standard_fantasy_points": row.fantasy_points_standard_10/10,
+                    "ppr_fantasy_points": row.fantasy_points_ppr_10/10
+                }
+            )
+
+    # return search object
+    return {
+        "previous": prev_token,
+        "next": next_token,
+        "results": json
+    }
+
 @router.get("/{player_id}/", response_model=PlayerStatsResponse)
 def get_player_statistics(player_id: str):
     with db.engine.begin() as connection:
+        # query database
         stats = connection.execute(sqlalchemy.text("""
             SELECT player_id, year, age, position, team, games_played, games_started,
                    passing_yards, passing_tds, interceptions, rushing_atts, rushing_yards,
@@ -26,9 +160,11 @@ def get_player_statistics(player_id: str):
             WHERE player_id = :player_id
         """), {'player_id': player_id}).mappings().fetchall()
         
+        # if no player statistics are found
         if not stats:
             raise HTTPException(status_code=404, detail="Player statistics not found")
 
+        # format returned stats
         formatted_stats = [{
             'player_id': row['player_id'],
             'year': row['year'],
@@ -55,24 +191,23 @@ def get_player_statistics(player_id: str):
             'two_point_conversions_passing': row['two_point_conversions_passing']
         } for row in stats]
 
+        # print formatted_stats for testing
+        print(f"Statistics : {formatted_stats}")
+
+        # return formatted_stats
         return {"player_stats": {"history": formatted_stats}}
-
-
-class DraftPlayerRequest(BaseModel):
-    team_id: int
 
 @router.post("/{player_id}/draft")
 def draft_player(player_id: str, request: DraftPlayerRequest):
     with db.engine.begin() as connection:
+        # update database with draft selection
         try:
-            result = connection.execute(sqlalchemy.text("""
+            connection.execute(sqlalchemy.text("""
                 INSERT INTO selections (team_id, player_id, when_selected)
-                VALUES (:team_id, :player_id, EXTRACT(EPOCH FROM NOW()))
-                RETURNING team_id
-            """), {'team_id': request.team_id, 'player_id': player_id})
+                VALUES (:team_id, :player_id, EXTRACT(EPOCH FROM NOW()))"""), 
+                {'team_id': request.team_id, 'player_id': player_id})
 
-            drafted_team_id = result.scalar_one()
-            return {"success": True}
+            return "OK"
+        # if the player is already drafted or the team is not found
         except sqlalchemy.exc.IntegrityError as e:
             raise HTTPException(status_code=409, detail="Player already drafted or team not found")
-
--- src/api/server.py
+++ src/api/server.py
@@ -1,44 +1,22 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import catalog, admin, drafts, info, inventory, players, teams
+from src.api import drafts, players, teams
 import json
 import logging
-import sys
-from starlette.middleware.cors import CORSMiddleware
 
 description = """
-Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+Mock Master is the premier drafting site for all your fantasy football desires.
 """
 
 app = FastAPI(
-    title="Central Coast Cauldrons",
-    description=description,
-    version="0.0.1",
-    terms_of_service="http://example.com/terms/",
-    contact={
-        "name": "Lucas Pierce",
-        "email": "lupierce@calpoly.edu",
-    },
+    title="Mock Master",
+    description=description
 )
 
-origins = ["https://potion-exchange.vercel.app"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "OPTIONS"],
-    allow_headers=["*"],
-)
-
-app.include_router(inventory.router)
 app.include_router(teams.router)
-app.include_router(catalog.router)
 app.include_router(drafts.router)
 app.include_router(players.router)
-app.include_router(admin.router)
-app.include_router(info.router)
 
 @app.exception_handler(exceptions.RequestValidationError)
 @app.exception_handler(ValidationError)
@@ -53,4 +31,4 @@ async def validation_exception_handler(request, exc):
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Central Coast Cauldrons."}
+    return {"message": "Welcome to MockMaster."}
