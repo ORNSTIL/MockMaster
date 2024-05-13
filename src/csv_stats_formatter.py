09735d589c5d92eefb9d8b30868bc74781256be5
Xavier Garcia <xmangarcia17@gmail.com>
2024-05-13 13:49:52 -0700

    edit csv_formatter

--- src/csv_stats_formatter.py
+++ src/csv_stats_formatter.py
@@ -1,8 +1,8 @@
 
 
-def main():
-    fin = open("2023_player_stats_old.csv", "r")
-    fout = open("2023_player_stats.csv", "w")
+def main(file_in, file_out):
+    fin = open(file_in, "r")
+    fout = open(file_out, "w")
     
     line = fin.readline()
     fout.write(line)
@@ -16,9 +16,14 @@ def main():
         
     line = fin.readline()
     line = line.split(',')
+
+    player_names = {}
+    repeated_players = {}
     
     print(fan_std_col, fan_ppr_col)
-    while line != None:
+    count = 0
+    while line != "" or line != None:
+        count += 1
         if len(line) > 1:
             name = line[0]
             if len(name) > 2:
@@ -26,6 +31,13 @@ def main():
                     name = name[:-1]
                 if name[-1] == '+' or name[-1] == '*':
                     name = name[:-1]
+            if name not in player_names:
+                player_names[name] = 1
+            else:
+                if name not in repeated_players:
+                    repeated_players[name] = 1
+                else:
+                    repeated_players[name] += 1
             new_line = name
             new_line += ","
             for i in range(1, len(line)):
@@ -39,9 +51,18 @@ def main():
             new_line = new_line[:-1]
             fout.write(new_line)
             line = fin.readline()
+            if line == "" or None:
+                break
             line = line.split(',')
+
+    for name in repeated_players.keys():
+        print(f"{name} repeated {repeated_players[name]} times")
+
+    print("DONE")
+    print("number of lines: ", count)
+
     fout.close()
     fin.close()
 
 if __name__=="__main__":
-    main()
+    main("2022_player_stats_old.txt", "2022_player_stats.csv")
