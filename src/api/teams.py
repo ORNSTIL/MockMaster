58031199635840e6378a5ef05236a56a6f3ad11d
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-05-22 15:39:36 -0700

    Update teams.py

--- src/api/teams.py
+++ src/api/teams.py
@@ -16,25 +16,20 @@ class TeamUpdateRequest(BaseModel):
 @router.put("/{team_id}/")
 def update_team_name(team_id: int, update_request: TeamUpdateRequest):
     with db.engine.begin() as connection:
-        # update the team's name
-        result = connection.execute(sqlalchemy.text("""UPDATE teams
+        updated_team_id = connection.execute(sqlalchemy.text("""UPDATE teams
                                                        SET team_name = :team_name
                                                        WHERE team_id = :team_id"""),
-                                    {"team_name": update_request.team_name, "team_id": team_id})
-        
-        # if no team is found
-        updated_team_id = result.scalar_one_or_none()
+                                    {"team_name": update_request.team_name, "team_id": team_id}).scalar_one_or_none()
         if not updated_team_id:
             raise HTTPException(status_code=404, detail="Team not found")
 
-    # exit status
     return "OK"
 
 
 @router.get("/{team_id}")
 def get_team(team_id: int):
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text(""" SELECT when_selected, position, player_name 
+        team_info = connection.execute(sqlalchemy.text(""" SELECT when_selected, position, player_name 
                                                         FROM selections
                                                         JOIN players on selections.player_id = players.player_id
                                                         JOIN stats on players.player_id = stats.player_id
@@ -43,7 +38,7 @@ def get_team(team_id: int):
                                                        """),
                                                     {"team_id": team_id})
     team = []
-    for row in result:
+    for row in team_info:
         team.append({"selection": row.when_selected, "position": row.position, "player": row.player_name})
         
     return team
